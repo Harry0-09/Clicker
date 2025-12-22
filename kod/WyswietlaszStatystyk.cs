dae970ba896fd29a0478487c3a709d370618cad1
Jan <jan.dmoch@gmail.com>
2025-12-22 11:01:27 +0100

    WyswietlaszStatystyk.cs
    Oryginał

---
+++ kod/WyswietlaszStatystyk.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using TMPro;
+using System.Net.WebSockets;
+
+public class WyswietlaszStatystyk : MonoBehaviour
+{
+    public TextMeshProUGUI statsText;
+    public LiczenieCzasu playTime; 
+
+    void Update()
+    {
+        float czasSesji = GameManager.Instance.sessionTime;
+        float clicks = GameManager.Instance.klikniecia;
+        float kasa = GameManager.Instance.kasaCalkowita;
+
+        int hours2 = Mathf.FloorToInt(czasSesji/ 3600);
+        int minutes2 = Mathf.FloorToInt((czasSesji % 3600) / 60);
+        int seconds2 = Mathf.FloorToInt(czasSesji % 60);
+
+        statsText.text =
+            $"Czas sesji: {hours2:00}:{minutes2:00}:{seconds2:00}\n" +
+            $"Całkowite zebrane pieniądze: {kasa} \n" +
+            $"Kliknięcia: {clicks}";
+    }
+}

ea3ad16314c5781d4cfdeca8faf8642454551636
Jan <jan.dmoch@gmail.com>
2025-12-22 11:00:59 +0100

    Upgrades.cs
    Oryginał

---
+++ kod/Upgrades.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+using UnityEngine.UI;
+public class Upgrades : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public Text upgradeText;
+    public Text pointsText;
+    void Start()
+    {
+        UpdateUI();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public void Mnoznik()
+    {
+        if (GameManager.Instance.money >= GameManager.Instance.cena)
+        {
+            Debug.Log("Kupilesz ulepszenie"); 
+            GameManager.Instance.money = GameManager.Instance.money - GameManager.Instance.cena;
+            GameManager.Instance.cena = GameManager.Instance.cena * GameManager.Instance.mnoznik_ceny;
+            GameManager.Instance.mnoznik_klikniec = GameManager.Instance.mnoznik_klikniec * 1.2f;
+            pointsText.text = pointsText.text = "money " + Mathf.RoundToInt(GameManager.Instance.money);
+            UpdateUI();
+        }
+        else
+            Debug.Log("Nie masz kaski");
+
+    }
+    void UpdateUI()
+    {
+        upgradeText.text = "price " + Mathf.RoundToInt(GameManager.Instance.cena).ToString()
+        + "\n" + "multiplier " + (GameManager.Instance.mnoznik_klikniec).ToString("F1");
+        pointsText.text = pointsText.text = "money " + Mathf.RoundToInt(GameManager.Instance.money);
+
+    }
+
+}

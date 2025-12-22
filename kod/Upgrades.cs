3eb9b3cfc11359491d62ef549ce4b846fe7fd660
Jan <jan.dmoch@gmail.com>
2025-12-22 11:37:25 +0100

    Upgrades1.2.cs

--- kod/Upgrades.cs
+++ kod/Upgrades.cs
@@ -34,7 +34,8 @@ public class Upgrades : MonoBehaviour
     {
         upgradeText.text = "price " + Mathf.RoundToInt(GameManager.Instance.cena).ToString()
         + "\n" + "multiplier " + (GameManager.Instance.mnoznik_klikniec).ToString("F1");
-        pointsText.text = pointsText.text = "money " + Mathf.RoundToInt(GameManager.Instance.money);
+
+        pointsText.text = pointsText.text = "money " + MoneyFormatter.Format(Mathf.RoundToInt(GameManager.Instance.money));
 
     }
 
